22a1eb75d5f6ff65f12011f2bb5816985b100065
XYZboom <xyzboom@qq.com>
2025-01-04 23:55:25 +0800

    feat(scala3): report scala3-22310

---
+++ Scala3/scala3-22310/0000019431edd882/main.scala
@@ -0,0 +1,228 @@
+// JVM_DEFAULT_MODE: all
+// TARGET_BACKEND: JVM
+// JVM_TARGET: 1.8
+// FILE: BKP7.scala
+class BKP7 extends Object {
+  def za2P(juPjEjU4: BKP7, kGuCNHs: BKP7): Object = 
+    ???
+  def qADqh9(yDTyxR: Object, pw10T: Object, izv: BKP7): Object = 
+    ???
+}
+
+// FILE: JEd0tJ.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public final class JEd0tJ extends Object  {
+    public /*@NotNull*/ JEd0tJ mOP3Glg9(/*@Nullable*/ JEd0tJ mET, /*@NotNull*/ Object tcm, /*@NotNull*/ Object oPfCVor) {
+        throw new RuntimeException();
+    }
+    public /*@NotNull*/ void vBMR1jic(/*@NotNull*/ Object bLaBZt, /*@NotNull*/ BKP7 tm95) {
+        throw new RuntimeException();
+    }
+    public final @NotNull JEd0tJ mtT(@Nullable Object tVYMKQtY, @NotNull JEd0tJ fXZ2053g, @NotNull JEd0tJ cMkF) {
+        throw new RuntimeException();
+    }
+}
+
+// FILE: O75ZVEt.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public abstract class O75ZVEt extends Object  {
+    public /*@Nullable*/ BKP7 oHy5c(/*@NotNull*/ JEd0tJ pGiU8l, /*@NotNull*/ JEd0tJ zDLneE, /*@NotNull*/ O75ZVEt lrCg2) {
+        throw new RuntimeException();
+    }
+    public @Nullable Object nWF8(@NotNull JEd0tJ lTZjEOa, @NotNull Object pQf5DkU7) {
+        throw new RuntimeException();
+    }
+}
+
+// FILE: IJn.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public abstract class IJn</*@NotNull*/ T0, /*@NotNull*/ T1> extends O75ZVEt  {
+    public abstract /*@NotNull*/ T1 ecgodv();
+    public abstract /*@Nullable*/ Object o0VALOTT(/*@NotNull*/ IJn</*@NotNull*/ JEd0tJ, /*@NotNull*/ Void> zeGy, /*@NotNull*/ T1 j2K1iHUY, /*@Nullable*/ O75ZVEt eD4fxn3);
+    public abstract /*@Nullable*/ Object rwWJ(/*@NotNull*/ T0 dNwEPne, /*@NotNull*/ BKP7 a59OzyYq, /*@NotNull*/ BKP7 bl2Y);
+    // stub
+    /*
+    public @Nullable BKP7 oHy5c(@NotNull JEd0tJ pGiU8l, @NotNull JEd0tJ zDLneE, @NotNull O75ZVEt lrCg2) {
+        throw new RuntimeException();
+    }
+    */
+    public final /*@Nullable*/ Object nWF8(/*@NotNull*/ JEd0tJ lTZjEOa, /*@NotNull*/ Object pQf5DkU7) {
+        throw new RuntimeException();
+    }
+}
+
+// FILE: UOll5qR.scala
+class UOll5qR extends Object {
+  def veT(mk7z: BKP7, mJo2J7J: IJn[Object, Object]): BKP7 = 
+    ???
+}
+
+// FILE: UwurxJq.scala
+abstract class UwurxJq[T2, T3] extends IJn[Object, Void] {
+  def v36(wfrV6: JEd0tJ, ix2IKK7: T3): BKP7
+  def fvAlu(tbyCFwt: Object, iPfs2aK: IJn[UOll5qR, Void]): Unit = 
+    ???
+  override def ecgodv(): Void = 
+    ???
+  override def o0VALOTT(zeGy: IJn[Void, Void], j2K1iHUY: Void, eD4fxn3: O75ZVEt): Object = 
+    ???
+  override def rwWJ(dNwEPne: Object, a59OzyYq: BKP7, bl2Y: BKP7): Object = 
+    ???
+  // stub
+  /*
+  override def nWF8(lTZjEOa: JEd0tJ, pQf5DkU7: Object): Object = 
+    ???
+  */
+  // stub
+  /*
+  override def oHy5c(pGiU8l: JEd0tJ, zDLneE: JEd0tJ, lrCg2: O75ZVEt): BKP7 = 
+    ???
+  */
+}
+
+// FILE: O2YeBtB.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public final class O2YeBtB extends UwurxJq<Object, Void>  {
+    public final /*@Nullable*/ O2YeBtB vORW6G(/*@NotNull*/ BKP7 kBb5Grvw, /*@Nullable*/ O2YeBtB hvik, /*@NotNull*/ O75ZVEt wmWKmo) {
+        throw new RuntimeException();
+    }
+    public final /*@NotNull*/ UwurxJq</*@NotNull*/ Object, /*@NotNull*/ JEd0tJ> aUljcW() {
+        throw new RuntimeException();
+    }
+    public final /*@Nullable*/ BKP7 dT2() {
+        throw new RuntimeException();
+    }
+    public /*@NotNull*/ BKP7 v36(/*@Nullable*/ JEd0tJ wfrV6, /*@NotNull*/ Void ix2IKK7) {
+        throw new RuntimeException();
+    }
+    // stub
+    /*
+    public final @NotNull void fvAlu(@Nullable Object tbyCFwt, @NotNull IJn<@NotNull UOll5qR, @NotNull Void> iPfs2aK) {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public final @Nullable Object nWF8(@NotNull JEd0tJ lTZjEOa, @NotNull Object pQf5DkU7) {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public @NotNull Void ecgodv() {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public @Nullable Object o0VALOTT(@NotNull IJn<@NotNull JEd0tJ, @NotNull Void> zeGy, @NotNull Void j2K1iHUY, @Nullable O75ZVEt eD4fxn3) {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public @Nullable Object rwWJ(@NotNull Object dNwEPne, @NotNull BKP7 a59OzyYq, @NotNull BKP7 bl2Y) {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public @Nullable BKP7 oHy5c(@NotNull JEd0tJ pGiU8l, @NotNull JEd0tJ zDLneE, @NotNull O75ZVEt lrCg2) {
+        throw new RuntimeException();
+    }
+    */
+}
+
+// FILE: M4mPO.scala
+abstract class M4mPO extends Object {
+  def f3l(): Object = 
+    ???
+  def lKJR(nSwEEEr: UOll5qR): UwurxJq[UOll5qR, O2YeBtB]
+}
+
+// FILE: BbU15h8.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public class BbU15h8</*@NotNull*/ T4, /*@NotNull*/ T5> extends IJn<Object, UOll5qR>  {
+    public @Nullable T5 q80fZBxl(@Nullable UOll5qR arp4uIZ, @Nullable Object zWxGu, @Nullable BbU15h8<Void, BKP7> zTD167) {
+        throw new RuntimeException();
+    }
+    public /*@NotNull*/ UwurxJq</*@NotNull*/ UOll5qR, /*@NotNull*/ O75ZVEt> yEg(/*@NotNull*/ BbU15h8</*@NotNull*/ O75ZVEt, /*@NotNull*/ O75ZVEt> jlQ, /*@NotNull*/ BbU15h8</*@NotNull*/ BKP7, /*@NotNull*/ O2YeBtB> cAvnaL, /*@NotNull*/ BKP7 klQlb) {
+        throw new RuntimeException();
+    }
+    public @NotNull UOll5qR ecgodv() {
+        throw new RuntimeException();
+    }
+    public /*@Nullable*/ Object o0VALOTT(/*@NotNull*/ IJn</*@NotNull*/ JEd0tJ, /*@NotNull*/ Void> zeGy, /*@NotNull*/ UOll5qR j2K1iHUY, /*@Nullable*/ O75ZVEt eD4fxn3) {
+        throw new RuntimeException();
+    }
+    public /*@Nullable*/ Object rwWJ(/*@NotNull*/ Object dNwEPne, /*@NotNull*/ BKP7 a59OzyYq, /*@NotNull*/ BKP7 bl2Y) {
+        throw new RuntimeException();
+    }
+    // stub
+    /*
+    public final @Nullable Object nWF8(@NotNull JEd0tJ lTZjEOa, @NotNull Object pQf5DkU7) {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public @Nullable BKP7 oHy5c(@NotNull JEd0tJ pGiU8l, @NotNull JEd0tJ zDLneE, @NotNull O75ZVEt lrCg2) {
+        throw new RuntimeException();
+    }
+    */
+}
+
+// FILE: FoTlvd.scala
+class FoTlvd[T6, T7] extends M4mPO {
+  def bnw(): FoTlvd[BKP7, O2YeBtB] = 
+    ???
+  def npWgJX3(cZ96: Object, aycARv2: BKP7, jUALy: Object): T7 = 
+    ???
+  def b7vvN(dKogMcD: Object): Object = 
+    ???
+  override def lKJR(nSwEEEr: UOll5qR): UwurxJq[UOll5qR, O2YeBtB] = 
+    ???
+  // stub
+  /*
+  override def f3l(): Object = 
+    ???
+  */
+}
+
+// FILE: JavaTopLevelContainer.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public final class JavaTopLevelContainer {
+    public static /*@NotNull*/ void uYh() {
+        throw new RuntimeException();
+    }
+}
+// FILE: NotNull.java
+package org.jetbrains.annotations;
+
+import java.lang.annotation.*;
+
+// org.jetbrains.annotations used in the compiler is version 13, whose @NotNull does not support the TYPE_USE target (version 15 does).
+// We're using our own @org.jetbrains.annotations.NotNull for testing purposes.
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
+public @interface NotNull {
+}
+// FILE: Nullable.java
+package org.jetbrains.annotations;
+
+import java.lang.annotation.*;
+
+// org.jetbrains.annotations used in the compiler is version 13, whose @Nullable does not support the TYPE_USE target (version 15 does).
+// We're using our own @org.jetbrains.annotations.Nullable for testing purposes.
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
+public @interface Nullable {
+}
