a7947b3595551f67ffd679c45227144f961fca68
XYZboom <xyzboom@qq.com>
2025-01-05 20:09:08 +0800

    feat(scala3): report scala3-22312

---
+++ Scala3/scala3-22312/0000019436427639/main.scala
@@ -0,0 +1,249 @@
+// JVM_DEFAULT_MODE: all
+// TARGET_BACKEND: JVM
+// JVM_TARGET: 1.8
+// FILE: UJ5LU69m.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import scala.runtime.BoxedUnit;
+public interface UJ5LU69m</*@NotNull*/ T0, /*@NotNull*/ T1, /*@NotNull*/ T2> {
+    public abstract /*@NotNull*/ Object vFeu6XdT();
+    public abstract /*@NotNull*/ UJ5LU69m</*@NotNull*/ Object, /*@NotNull*/ T1, /*@NotNull*/ T0> nsOvgY5b(/*@Nullable*/ T2 uHn2, /*@NotNull*/ UJ5LU69m</*@NotNull*/ T2, /*@NotNull*/ Void, /*@NotNull*/ T0> kYGxgw, /*@NotNull*/ UJ5LU69m</*@NotNull*/ T0, /*@NotNull*/ Object, /*@NotNull*/ T2> ywJyhEte);
+    public abstract /*@NotNull*/ void abxw(/*@Nullable*/ Object o7Xfba);
+}
+
+// FILE: NW49Iz.scala
+open class NW49Iz[T3, T4, T5] {
+  def m6io(uTiChawv: T3): T4 = 
+    ???
+}
+
+// FILE: UJdEDB7.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import scala.runtime.BoxedUnit;
+public interface UJdEDB7 extends UJ5LU69m<BoxedUnit, BoxedUnit, Object> {
+    public abstract /*@NotNull*/ NW49Iz</*@NotNull*/ Void, /*@NotNull*/ BoxedUnit, /*@NotNull*/ Object> xJpbeOn(/*@Nullable*/ Object wksPM, /*@NotNull*/ Object k6VZ);
+    public abstract /*@NotNull*/ NW49Iz</*@NotNull*/ Object, /*@NotNull*/ Object, /*@NotNull*/ Object> bznT4Bua();
+    public default @NotNull Object vFeu6XdT() {
+        throw new RuntimeException();
+    }
+    public default /*@NotNull*/ UJ5LU69m</*@NotNull*/ Object, /*@NotNull*/ BoxedUnit, /*@NotNull*/ BoxedUnit> nsOvgY5b(/*@Nullable*/ Object uHn2, /*@NotNull*/ UJ5LU69m</*@NotNull*/ Object, /*@NotNull*/ Void, /*@NotNull*/ BoxedUnit> kYGxgw, /*@NotNull*/ UJ5LU69m</*@NotNull*/ BoxedUnit, /*@NotNull*/ Object, /*@NotNull*/ Object> ywJyhEte) {
+        throw new RuntimeException();
+    }
+    public default @NotNull void abxw(@Nullable Object o7Xfba) {
+        throw new RuntimeException();
+    }
+}
+
+// FILE: KdI.scala
+class KdI {
+  def e4yqs5(xWqNoIQ: KdI, eBV: UJdEDB7, bQ71mMrJ: NW49Iz[Void, Void, KdI]): Unit = 
+    ???
+  // stub
+  /*
+  override def m6io(uTiChawv: Void): KdI = 
+    ???
+  */
+}
+
+// FILE: XazLG.scala
+trait XazLG[T6, T7] {
+  def rNR1L(rAraJ: NW49Iz[Void, T6, Unit]): KdI
+  def dlg(hDUOJrUD: NW49Iz[Unit, Unit, KdI], mnU: Object): XazLG[T7, KdI]
+  def bQ2(): XazLG[T7, Void]
+}
+
+// FILE: MtS9d4w.scala
+abstract class MtS9d4w extends NW49Iz[KdI, UJdEDB7, Unit] with UJdEDB7 with UJ5LU69m[Unit, Unit, Object] with XazLG[UJdEDB7, Object] {
+  def yu2h(rZpEUUN9: XazLG[UJdEDB7, Void], bWM: MtS9d4w, tx4Bs0s: NW49Iz[Object, Unit, UJdEDB7]): UJdEDB7 = 
+    ???
+  override def xJpbeOn(wksPM: Object, k6VZ: Object): NW49Iz[Void, Unit, Object] = 
+    ???
+  override def bznT4Bua(): NW49Iz[Object, Object, Object] = 
+    ???
+  override def rNR1L(rAraJ: NW49Iz[Void, UJdEDB7, Unit]): KdI = 
+    ???
+  override def dlg(hDUOJrUD: NW49Iz[Unit, Unit, KdI], mnU: Object): XazLG[Object, KdI] = 
+    ???
+  override def bQ2(): XazLG[Object, Void] = 
+    ???
+  override def vFeu6XdT(): Object = 
+    ???
+  override def nsOvgY5b(uHn2: Object, kYGxgw: UJ5LU69m[Object, Void, Unit], ywJyhEte: UJ5LU69m[Unit, Object, Object]): UJ5LU69m[Object, Unit, Unit]
+  // stub
+  /*
+  override def abxw(o7Xfba: Object): Unit = 
+    ???
+  */
+  // stub
+  /*
+  override def m6io(uTiChawv: KdI): UJdEDB7 = 
+    ???
+  */
+}
+
+// FILE: Fzo5r7WB.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import scala.runtime.BoxedUnit;
+public final class Fzo5r7WB extends MtS9d4w  {
+    public /*@Nullable*/ XazLG<MtS9d4w, MtS9d4w> ftGwViA(/*@Nullable*/ NW49Iz<Void, KdI, Void> vw2TMGH, /*@Nullable*/ NW49Iz<KdI, KdI, UJdEDB7> zZgkJej, /*@NotNull*/ UJ5LU69m</*@NotNull*/ UJdEDB7, /*@NotNull*/ BoxedUnit, /*@NotNull*/ BoxedUnit> dzZ) {
+        throw new RuntimeException();
+    }
+    public @Nullable MtS9d4w vWb6(@NotNull Object eCiY, @NotNull Fzo5r7WB kj9hgyD) {
+        throw new RuntimeException();
+    }
+    public final @NotNull Object irT() {
+        throw new RuntimeException();
+    }
+    public @NotNull UJ5LU69m<@NotNull Object, @NotNull BoxedUnit, @NotNull BoxedUnit> nsOvgY5b(@Nullable Object uHn2, @NotNull UJ5LU69m<@NotNull Object, @NotNull Void, @NotNull BoxedUnit> kYGxgw, @NotNull UJ5LU69m<@NotNull BoxedUnit, @NotNull Object, @NotNull Object> ywJyhEte) {
+        throw new RuntimeException();
+    }
+    public @NotNull void abxw(@Nullable Object o7Xfba) {
+        throw new RuntimeException();
+    }
+    // stub
+    /*
+    public final @NotNull UJdEDB7 yu2h(@Nullable XazLG<UJdEDB7, Void> rZpEUUN9, @Nullable MtS9d4w bWM, @Nullable NW49Iz<Object, BoxedUnit, UJdEDB7> tx4Bs0s) {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public final @NotNull NW49Iz<@NotNull Object, @NotNull Object, @NotNull Object> bznT4Bua() {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public final @Nullable KdI rNR1L(@Nullable NW49Iz<Void, UJdEDB7, BoxedUnit> rAraJ) {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public final @Nullable XazLG<Object, Void> bQ2() {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public final @NotNull Object vFeu6XdT() {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public @NotNull NW49Iz<@NotNull Void, @NotNull BoxedUnit, @NotNull Object> xJpbeOn(@Nullable Object wksPM, @NotNull Object k6VZ) {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public @NotNull XazLG<@NotNull Object, @NotNull KdI> dlg(@Nullable NW49Iz<BoxedUnit, BoxedUnit, KdI> hDUOJrUD, @Nullable Object mnU) {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public @Nullable UJdEDB7 m6io(@NotNull KdI uTiChawv) {
+        throw new RuntimeException();
+    }
+    */
+}
+
+// FILE: Xuwqi.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import scala.runtime.BoxedUnit;
+public abstract class Xuwqi extends NW49Iz<Object, UJdEDB7, MtS9d4w>  {
+    public abstract /*@Nullable*/ UJ5LU69m<Void, Object, Object> e4Yz();
+    public abstract /*@NotNull*/ UJ5LU69m</*@NotNull*/ KdI, /*@NotNull*/ UJdEDB7, /*@NotNull*/ UJdEDB7> rjdex(/*@NotNull*/ MtS9d4w wc3P6al, /*@NotNull*/ UJ5LU69m</*@NotNull*/ Fzo5r7WB, /*@NotNull*/ Xuwqi, /*@NotNull*/ Object> dVt1, /*@Nullable*/ UJ5LU69m<Object, KdI, Xuwqi> pVlee);
+    // stub
+    /*
+    public @Nullable UJdEDB7 m6io(@NotNull Object uTiChawv) {
+        throw new RuntimeException();
+    }
+    */
+}
+
+// FILE: ES7W2S.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import scala.runtime.BoxedUnit;
+public abstract class ES7W2S extends Object implements UJdEDB7, UJ5LU69m<BoxedUnit, BoxedUnit, Object>, XazLG<Xuwqi, Object> {
+    public abstract /*@Nullable*/ MtS9d4w yzNI(/*@NotNull*/ Xuwqi qZ7);
+    public abstract /*@NotNull*/ Xuwqi tfE8ub(/*@Nullable*/ Fzo5r7WB irxOoyPK, /*@NotNull*/ KdI poDDGs, /*@Nullable*/ Fzo5r7WB h5TXUbfg);
+    public /*@NotNull*/ NW49Iz</*@NotNull*/ Void, /*@NotNull*/ BoxedUnit, /*@NotNull*/ Object> xJpbeOn(/*@Nullable*/ Object wksPM, /*@NotNull*/ Object k6VZ) {
+        throw new RuntimeException();
+    }
+    public final @NotNull NW49Iz<@NotNull Object, @NotNull Object, @NotNull Object> bznT4Bua() {
+        throw new RuntimeException();
+    }
+    public final /*@Nullable*/ KdI rNR1L(/*@Nullable*/ NW49Iz<Void, Xuwqi, BoxedUnit> rAraJ) {
+        throw new RuntimeException();
+    }
+    public @NotNull XazLG<@NotNull Object, @NotNull KdI> dlg(@Nullable NW49Iz<BoxedUnit, BoxedUnit, KdI> hDUOJrUD, @Nullable Object mnU) {
+        throw new RuntimeException();
+    }
+    public @Nullable XazLG<Object, Void> bQ2() {
+        throw new RuntimeException();
+    }
+    public final @NotNull Object vFeu6XdT() {
+        throw new RuntimeException();
+    }
+    public abstract /*@NotNull*/ UJ5LU69m</*@NotNull*/ Object, /*@NotNull*/ BoxedUnit, /*@NotNull*/ BoxedUnit> nsOvgY5b(/*@Nullable*/ Object uHn2, /*@NotNull*/ UJ5LU69m</*@NotNull*/ Object, /*@NotNull*/ Void, /*@NotNull*/ BoxedUnit> kYGxgw, /*@NotNull*/ UJ5LU69m</*@NotNull*/ BoxedUnit, /*@NotNull*/ Object, /*@NotNull*/ Object> ywJyhEte);
+    public abstract /*@NotNull*/ void abxw(/*@Nullable*/ Object o7Xfba);
+}
+
+// FILE: T5b1m.scala
+trait T5b1m[T8, T9, T10] extends UJ5LU69m[KdI, Object, Xuwqi] with XazLG[Fzo5r7WB, Fzo5r7WB] {
+  def vpAo(rbTR0h: T8, ckxLLXsL: NW49Iz[UJdEDB7, ES7W2S, Xuwqi], jws: UJdEDB7): Fzo5r7WB
+  def yxsDbPqZ(zh9bj: MtS9d4w): T9
+  override def vFeu6XdT(): Object = 
+    ???
+  override def nsOvgY5b(uHn2: Xuwqi, kYGxgw: UJ5LU69m[Xuwqi, Void, KdI], ywJyhEte: UJ5LU69m[KdI, Object, Xuwqi]): UJ5LU69m[Object, Object, KdI] = 
+    ???
+  override def abxw(o7Xfba: Object): Unit = 
+    ???
+  override def rNR1L(rAraJ: NW49Iz[Void, Fzo5r7WB, Unit]): KdI = 
+    ???
+  override def dlg(hDUOJrUD: NW49Iz[Unit, Unit, KdI], mnU: Object): XazLG[Fzo5r7WB, KdI] = 
+    ???
+  override def bQ2(): XazLG[Fzo5r7WB, Void] = 
+    ???
+}
+
+// FILE: JavaTopLevelContainer.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import scala.runtime.BoxedUnit;
+public final class JavaTopLevelContainer {
+    public static /*@NotNull*/ void getAhCNp() {
+        throw new RuntimeException();
+    }
+}
+// FILE: NotNull.java
+package org.jetbrains.annotations;
+
+import java.lang.annotation.*;
+
+// org.jetbrains.annotations used in the compiler is version 13, whose @NotNull does not support the TYPE_USE target (version 15 does).
+// We're using our own @org.jetbrains.annotations.NotNull for testing purposes.
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
+public @interface NotNull {
+}
+// FILE: Nullable.java
+package org.jetbrains.annotations;
+
+import java.lang.annotation.*;
+
+// org.jetbrains.annotations used in the compiler is version 13, whose @Nullable does not support the TYPE_USE target (version 15 does).
+// We're using our own @org.jetbrains.annotations.Nullable for testing purposes.
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
+public @interface Nullable {
+}
---
+++ Scala3/scala3-22312/000001943648ec99/main.scala
@@ -0,0 +1,218 @@
+// JVM_DEFAULT_MODE: all
+// TARGET_BACKEND: JVM
+// JVM_TARGET: 1.8
+// FILE: OekYJuz.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import scala.runtime.BoxedUnit;
+public class OekYJuz extends Object  {
+    public final @Nullable void z04(@Nullable OekYJuz uzpV, @Nullable OekYJuz d7M, @NotNull OekYJuz oU5) {
+        throw new RuntimeException();
+    }
+    public /*@Nullable*/ void klU2Ge(/*@Nullable*/ OekYJuz vsw4dcVo, /*@Nullable*/ Object iolT, /*@Nullable*/ Object fDHeWn) {
+        throw new RuntimeException();
+    }
+}
+
+// FILE: C7Ec5rE.scala
+trait C7Ec5rE[T0, T1, T2] {
+  def wt9TQ(): Object
+  def vFTr(hso: T0, dAaMJC: OekYJuz): T0
+  def urp(ckaT: T1, zZFA: C7Ec5rE[Void, T2, Void], m7V: T0): T1
+}
+
+// FILE: CncQ.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import scala.runtime.BoxedUnit;
+public class CncQ extends Object implements C7Ec5rE<BoxedUnit, CncQ, OekYJuz> {
+    public final /*@Nullable*/ CncQ ehmEQ(/*@NotNull*/ OekYJuz eCBkRzg, /*@Nullable*/ Object eXZogx73) {
+        throw new RuntimeException();
+    }
+    public final /*@NotNull*/ Object xzH2j(/*@NotNull*/ Object kHWVPdI, /*@NotNull*/ C7Ec5rE</*@NotNull*/ CncQ, /*@NotNull*/ Void, /*@NotNull*/ OekYJuz> naQK7) {
+        throw new RuntimeException();
+    }
+    public final /*@NotNull*/ Object wt9TQ() {
+        throw new RuntimeException();
+    }
+    public final /*@NotNull*/ void vFTr(/*@Nullable*/ BoxedUnit hso, /*@NotNull*/ OekYJuz dAaMJC) {
+        throw new RuntimeException();
+    }
+    public final @Nullable CncQ urp(@NotNull CncQ ckaT, @Nullable C7Ec5rE<Void, OekYJuz, Void> zZFA, @NotNull BoxedUnit m7V) {
+        throw new RuntimeException();
+    }
+}
+
+// FILE: XgXYJ.scala
+trait XgXYJ extends C7Ec5rE[XgXYJ, XgXYJ, XgXYJ] {
+  def pzDBok(): C7Ec5rE[Void, Void, CncQ]
+  def yjeLnVZ(pki2fgJ: XgXYJ, jSxN: C7Ec5rE[OekYJuz, Unit, XgXYJ], iXY6Dv: XgXYJ): XgXYJ
+  def w3hce3R6(zd8W: C7Ec5rE[OekYJuz, Unit, Unit], bx0U: C7Ec5rE[OekYJuz, CncQ, XgXYJ]): Object
+  override def wt9TQ(): Object = 
+    ???
+  override def vFTr(hso: XgXYJ, dAaMJC: OekYJuz): XgXYJ = 
+    ???
+  override def urp(ckaT: XgXYJ, zZFA: C7Ec5rE[Void, XgXYJ, Void], m7V: XgXYJ): XgXYJ = 
+    ???
+}
+
+// FILE: C82A9sgh.scala
+open class C82A9sgh[T3] extends OekYJuz with XgXYJ with C7Ec5rE[XgXYJ, XgXYJ, XgXYJ] {
+  def aeax(d6w0H: Object): OekYJuz = 
+    ???
+  override def pzDBok(): C7Ec5rE[Void, Void, CncQ] = 
+    ???
+  override def yjeLnVZ(pki2fgJ: XgXYJ, jSxN: C7Ec5rE[OekYJuz, Unit, XgXYJ], iXY6Dv: XgXYJ): XgXYJ = 
+    ???
+  override def w3hce3R6(zd8W: C7Ec5rE[OekYJuz, Unit, Unit], bx0U: C7Ec5rE[OekYJuz, CncQ, XgXYJ]): Object = 
+    ???
+  // stub
+  /*
+  override def z04(uzpV: OekYJuz, d7M: OekYJuz, oU5: OekYJuz): Unit = 
+    ???
+  */
+  // stub
+  /*
+  override def wt9TQ(): Object = 
+    ???
+  */
+  // stub
+  /*
+  override def vFTr(hso: XgXYJ, dAaMJC: OekYJuz): XgXYJ = 
+    ???
+  */
+  // stub
+  /*
+  override def urp(ckaT: XgXYJ, zZFA: C7Ec5rE[Void, XgXYJ, Void], m7V: XgXYJ): XgXYJ = 
+    ???
+  */
+  // stub
+  /*
+  override def klU2Ge(vsw4dcVo: OekYJuz, iolT: Object, fDHeWn: Object): Unit = 
+    ???
+  */
+}
+
+// FILE: YKmZbf.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import scala.runtime.BoxedUnit;
+public class YKmZbf extends Object implements C7Ec5rE<YKmZbf, CncQ, Void> {
+    public /*@Nullable*/ void vLB() {
+        throw new RuntimeException();
+    }
+    public final /*@Nullable*/ YKmZbf keV0(/*@NotNull*/ Object wD6yg) {
+        throw new RuntimeException();
+    }
+    public /*@NotNull*/ Object wt9TQ() {
+        throw new RuntimeException();
+    }
+    public @NotNull YKmZbf vFTr(@Nullable YKmZbf hso, @NotNull OekYJuz dAaMJC) {
+        throw new RuntimeException();
+    }
+    public /*@Nullable*/ CncQ urp(/*@NotNull*/ CncQ ckaT, /*@Nullable*/ C7Ec5rE<Void, Void, Void> zZFA, /*@NotNull*/ YKmZbf m7V) {
+        throw new RuntimeException();
+    }
+}
+
+// FILE: Azj3xo.scala
+class Azj3xo extends Object with XgXYJ with C7Ec5rE[XgXYJ, XgXYJ, XgXYJ] {
+  def q95Y7Ukv(): C7Ec5rE[Azj3xo, CncQ, OekYJuz] = 
+    ???
+  override def pzDBok(): C7Ec5rE[Void, Void, CncQ] = 
+    ???
+  override def yjeLnVZ(pki2fgJ: XgXYJ, jSxN: C7Ec5rE[OekYJuz, Unit, XgXYJ], iXY6Dv: XgXYJ): XgXYJ = 
+    ???
+  override def w3hce3R6(zd8W: C7Ec5rE[OekYJuz, Unit, Unit], bx0U: C7Ec5rE[OekYJuz, CncQ, XgXYJ]): Object = 
+    ???
+  // stub
+  /*
+  override def wt9TQ(): Object = 
+    ???
+  */
+  // stub
+  /*
+  override def vFTr(hso: XgXYJ, dAaMJC: OekYJuz): XgXYJ = 
+    ???
+  */
+  // stub
+  /*
+  override def urp(ckaT: XgXYJ, zZFA: C7Ec5rE[Void, XgXYJ, Void], m7V: XgXYJ): XgXYJ = 
+    ???
+  */
+}
+
+// FILE: A6dtwIC.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import scala.runtime.BoxedUnit;
+public abstract class A6dtwIC extends CncQ implements C7Ec5rE<BoxedUnit, CncQ, OekYJuz> {
+    public final /*@NotNull*/ A6dtwIC hMsc(/*@Nullable*/ C7Ec5rE<OekYJuz, Object, Void> mpC, /*@Nullable*/ OekYJuz mWbe, /*@NotNull*/ OekYJuz tsBSR) {
+        throw new RuntimeException();
+    }
+    public abstract /*@Nullable*/ A6dtwIC i1NBB(/*@NotNull*/ XgXYJ esEwNqV, /*@NotNull*/ Azj3xo zm5tF);
+    // stub
+    /*
+    public final @NotNull Object wt9TQ() {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public final @NotNull void vFTr(@Nullable BoxedUnit hso, @NotNull OekYJuz dAaMJC) {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public final @Nullable CncQ urp(@NotNull CncQ ckaT, @Nullable C7Ec5rE<Void, OekYJuz, Void> zZFA, @NotNull BoxedUnit m7V) {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public final @Nullable CncQ ehmEQ(@NotNull OekYJuz eCBkRzg, @Nullable Object eXZogx73) {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public final @NotNull Object xzH2j(@NotNull Object kHWVPdI, @NotNull C7Ec5rE<@NotNull CncQ, @NotNull Void, @NotNull OekYJuz> naQK7) {
+        throw new RuntimeException();
+    }
+    */
+}
+
+// FILE: JavaTopLevelContainer.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import scala.runtime.BoxedUnit;
+public final class JavaTopLevelContainer {
+    public static @Nullable Void getNyQbrL0() {
+        throw new RuntimeException();
+    }
+}
+// FILE: NotNull.java
+package org.jetbrains.annotations;
+
+import java.lang.annotation.*;
+
+// org.jetbrains.annotations used in the compiler is version 13, whose @NotNull does not support the TYPE_USE target (version 15 does).
+// We're using our own @org.jetbrains.annotations.NotNull for testing purposes.
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
+public @interface NotNull {
+}
+// FILE: Nullable.java
+package org.jetbrains.annotations;
+
+import java.lang.annotation.*;
+
+// org.jetbrains.annotations used in the compiler is version 13, whose @Nullable does not support the TYPE_USE target (version 15 does).
+// We're using our own @org.jetbrains.annotations.Nullable for testing purposes.
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
+public @interface Nullable {
+}
