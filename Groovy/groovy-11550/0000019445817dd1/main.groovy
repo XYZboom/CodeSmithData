85465a69dde97922b0e29aa32bccbfcca9bc6d09
XYZboom <xyzboom@qq.com>
2025-01-09 10:37:25 +0800

    feat(groovy): report groovy-11550

---
+++ Groovy/groovy-11550/0000019445817dd1/main.groovy
@@ -0,0 +1,195 @@
+// JVM_DEFAULT_MODE: all
+// TARGET_BACKEND: JVM
+// JVM_TARGET: 1.8
+// FILE: C0N.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public class C0N extends Object  {
+    public final /*@NotNull*/ void xD1wV(/*@NotNull*/ Object rpa, /*@Nullable*/ C0N e8y6g2nK, /*@Nullable*/ Object jL1ty) {
+        throw new RuntimeException();
+    }
+    public final /*@Nullable*/ void zc6NqFI4(/*@Nullable*/ C0N zSPvjBG, /*@NotNull*/ C0N fYqSK, /*@NotNull*/ Object x6PZDRpO) {
+        throw new RuntimeException();
+    }
+    public final /*@Nullable*/ void llMilAU(/*@NotNull*/ Object mMvTg, /*@Nullable*/ C0N mFXrdvF, /*@NotNull*/ Object hDof1) {
+        throw new RuntimeException();
+    }
+}
+
+// FILE: P4dlK1.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public final class P4dlK1</*@NotNull*/ T0, /*@NotNull*/ T1, /*@NotNull*/ T2> extends Object  {
+    public /*@Nullable*/ Object zW00tY(/*@NotNull*/ P4dlK1</*@NotNull*/ Void, /*@NotNull*/ T2, /*@NotNull*/ Void> ySEuF, /*@Nullable*/ T0 jsetNgDz) {
+        throw new RuntimeException();
+    }
+}
+
+// FILE: PdK0.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public abstract class PdK0 extends Object  {
+    public abstract /*@NotNull*/ void pRJpYV17(/*@Nullable*/ C0N lSwrlt, /*@NotNull*/ Object uPzy);
+    public abstract /*@Nullable*/ P4dlK1<PdK0, Object, PdK0> dZM7y(/*@NotNull*/ C0N dykAErc, /*@NotNull*/ P4dlK1</*@NotNull*/ Void, /*@NotNull*/ Void, /*@NotNull*/ PdK0> cN4);
+}
+
+// FILE: Nmjj.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public abstract class Nmjj extends Object  {
+    public final /*@NotNull*/ PdK0 q0nvt1() {
+        throw new RuntimeException();
+    }
+}
+
+// FILE: YQ7.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public final class YQ7 extends C0N  {
+    public /*@NotNull*/ PdK0 ulmN(/*@Nullable*/ YQ7 lzOIu) {
+        throw new RuntimeException();
+    }
+    // stub
+    /*
+    public final @NotNull void xD1wV(@NotNull Object rpa, @Nullable C0N e8y6g2nK, @Nullable Object jL1ty) {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public final @Nullable void zc6NqFI4(@Nullable C0N zSPvjBG, @NotNull C0N fYqSK, @NotNull Object x6PZDRpO) {
+        throw new RuntimeException();
+    }
+    */
+    // stub
+    /*
+    public final @Nullable void llMilAU(@NotNull Object mMvTg, @Nullable C0N mFXrdvF, @NotNull Object hDof1) {
+        throw new RuntimeException();
+    }
+    */
+}
+
+// FILE: Xoo.groovy
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public interface Xoo {
+    public abstract /*@NotNull*/ YQ7 qrOM(/*@Nullable*/ YQ7 fxx4RyrY);
+    public abstract /*@NotNull*/ Nmjj r21(/*@Nullable*/ YQ7 kkiJsCJS, /*@NotNull*/ Xoo uvA, /*@Nullable*/ PdK0 ckrZ);
+    public abstract /*@Nullable*/ YQ7 zyAV();
+}
+
+// FILE: R7e.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public interface R7e</*@NotNull*/ T3, /*@NotNull*/ T4> {
+    public abstract /*@Nullable*/ PdK0 ubRxUS8(/*@NotNull*/ C0N oxJYehG5, /*@NotNull*/ R7e</*@NotNull*/ Object, /*@NotNull*/ Void> a4NVPPiI, /*@NotNull*/ Nmjj lqh);
+    public abstract /*@NotNull*/ PdK0 ajlh(/*@NotNull*/ Object hf62iJ);
+    public abstract /*@NotNull*/ P4dlK1</*@NotNull*/ C0N, /*@NotNull*/ PdK0, /*@NotNull*/ Object> q1by(/*@NotNull*/ P4dlK1</*@NotNull*/ T3, /*@NotNull*/ T4, /*@NotNull*/ T3> kPjDt, /*@NotNull*/ P4dlK1</*@NotNull*/ T4, /*@NotNull*/ Nmjj, /*@NotNull*/ C0N> crjZIQx);
+}
+
+// FILE: J4PVjpck.groovy
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public abstract class J4PVjpck extends Object implements Xoo, R7e<C0N, PdK0> {
+    public /*@NotNull*/ PdK0 fnf6GR(/*@NotNull*/ PdK0 xSuWL, /*@Nullable*/ J4PVjpck zBV) {
+        throw new RuntimeException();
+    }
+    public /*@NotNull*/ P4dlK1</*@NotNull*/ YQ7, /*@NotNull*/ C0N, /*@NotNull*/ Xoo> d71(/*@NotNull*/ C0N ir4ny4pU) {
+        throw new RuntimeException();
+    }
+    public final /*@Nullable*/ Object cJcTT3r4(/*@Nullable*/ R7e<C0N, Nmjj> hKEeFsST, /*@Nullable*/ R7e<Void, PdK0> jLbKI, /*@NotNull*/ Object teijTI) {
+        throw new RuntimeException();
+    }
+    public /*@NotNull*/ YQ7 qrOM(/*@Nullable*/ YQ7 fxx4RyrY) {
+        throw new RuntimeException();
+    }
+    public final /*@NotNull*/ Nmjj r21(/*@Nullable*/ YQ7 kkiJsCJS, /*@NotNull*/ Xoo uvA, /*@Nullable*/ PdK0 ckrZ) {
+        throw new RuntimeException();
+    }
+    public /*@Nullable*/ YQ7 zyAV() {
+        throw new RuntimeException();
+    }
+    public final /*@Nullable*/ PdK0 ubRxUS8(/*@NotNull*/ C0N oxJYehG5, /*@NotNull*/ R7e</*@NotNull*/ Object, /*@NotNull*/ HGI> a4NVPPiI, /*@NotNull*/ Nmjj lqh) {
+        throw new RuntimeException();
+    }
+    public /*@NotNull*/ PdK0 ajlh(/*@NotNull*/ Object hf62iJ) {
+        throw new RuntimeException();
+    }
+    public /*@NotNull*/ P4dlK1</*@NotNull*/ C0N, /*@NotNull*/ PdK0, /*@NotNull*/ Object> q1by(/*@NotNull*/ P4dlK1</*@NotNull*/ C0N, /*@NotNull*/ PdK0, /*@NotNull*/ C0N> kPjDt, /*@NotNull*/ P4dlK1</*@NotNull*/ PdK0, /*@NotNull*/ Nmjj, /*@NotNull*/ C0N> crjZIQx) {
+        throw new RuntimeException();
+    }
+}
+
+// FILE: HGI.groovy
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public abstract class HGI extends Nmjj implements R7e<HGI, HGI>, Xoo {
+    public abstract /*@NotNull*/ C0N yXK9qrr(/*@NotNull*/ Nmjj cHliGUOk);
+    public /*@NotNull*/ void pdI(/*@NotNull*/ HGI m7Pr6Z9p) {
+        throw new RuntimeException();
+    }
+    public abstract /*@Nullable*/ PdK0 acXxO1(/*@NotNull*/ Object qCzP, /*@NotNull*/ Xoo ck1r3mc);
+    public /*@Nullable*/ PdK0 ubRxUS8(/*@NotNull*/ C0N oxJYehG5, /*@NotNull*/ R7e</*@NotNull*/ Object, /*@NotNull*/ Void> a4NVPPiI, /*@NotNull*/ Nmjj lqh) {
+        throw new RuntimeException();
+    }
+    public final /*@NotNull*/ PdK0 ajlh(/*@NotNull*/ Object hf62iJ) {
+        throw new RuntimeException();
+    }
+    public /*@NotNull*/ P4dlK1</*@NotNull*/ C0N, /*@NotNull*/ PdK0, /*@NotNull*/ Object> q1by(/*@NotNull*/ P4dlK1</*@NotNull*/ HGI, /*@NotNull*/ HGI, /*@NotNull*/ HGI> kPjDt, /*@NotNull*/ P4dlK1</*@NotNull*/ HGI, /*@NotNull*/ Nmjj, /*@NotNull*/ C0N> crjZIQx) {
+        throw new RuntimeException();
+    }
+    public /*@NotNull*/ YQ7 qrOM(/*@Nullable*/ YQ7 fxx4RyrY) {
+        throw new RuntimeException();
+    }
+    public final /*@NotNull*/ Nmjj r21(/*@Nullable*/ YQ7 kkiJsCJS, /*@NotNull*/ Xoo uvA, /*@Nullable*/ PdK0 ckrZ) {
+        throw new RuntimeException();
+    }
+    public /*@Nullable*/ YQ7 zyAV() {
+        throw new RuntimeException();
+    }
+    // stub
+    /*
+    public final @NotNull PdK0 q0nvt1() {
+        throw new RuntimeException();
+    }
+    */
+}
+
+// FILE: JavaTopLevelContainer.java
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+public final class JavaTopLevelContainer {
+    public static /*@Nullable*/ Nmjj t3ktQG(/*@Nullable*/ Xoo dBhucE, /*@NotNull*/ Xoo nVRZ4VR) {
+        throw new RuntimeException();
+    }
+    public static /*@NotNull*/ PdK0 hNP(/*@NotNull*/ C0N nOXFJ, /*@Nullable*/ Object wkE, /*@Nullable*/ C0N rMzM) {
+        throw new RuntimeException();
+    }
+    public static /*@NotNull*/ C0N getMT00vH() {
+        throw new RuntimeException();
+    }
+}
+// FILE: NotNull.java
+package org.jetbrains.annotations;
+
+import java.lang.annotation.*;
+
+// org.jetbrains.annotations used in the compiler is version 13, whose @NotNull does not support the TYPE_USE target (version 15 does).
+// We're using our own @org.jetbrains.annotations.NotNull for testing purposes.
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
+public @interface NotNull {
+}
+// FILE: Nullable.java
+package org.jetbrains.annotations;
+
+import java.lang.annotation.*;
+
+// org.jetbrains.annotations used in the compiler is version 13, whose @Nullable does not support the TYPE_USE target (version 15 does).
+// We're using our own @org.jetbrains.annotations.Nullable for testing purposes.
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
+public @interface Nullable {
+}
